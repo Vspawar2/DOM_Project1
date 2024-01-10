563448b1419c56a82148c35f029d72cc279dc576
vishal <pawarvishal8590@gmail.com>
2024-01-10 12:09:35 +0530

    first commit

---
+++ srcipt.js
@@ -0,0 +1,9 @@
+
+var targetElement = document.getElementById("targetDiv");
+    targetElement.style.backgroundColor = "yellow";
+    targetElement.style.margin = "20px";
+    targetElement.style.padding = "10px";
+    targetElement.style.fontSize = "18px";
+    targetElement.style.fontWeight = "bold";
+    targetElement.style.height = "200px";
+    targetElement.style.width = "300px";
